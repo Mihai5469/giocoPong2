0efb6ed3b475c609838bfaaf35210c7f90ce6f96
MIHAI IACHIMOVSCHI <MIHAI.IACHIMOVSCHI@ptpvenezia.edu.it>
2023-05-04 15:29:56 +0200

    Aggiungere i file di progetto.

---
+++ giocoPong2.cpp
@@ -0,0 +1,12 @@
+// giocoPong2.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
